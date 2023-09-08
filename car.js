3d9d81043f44e18a41cf17c9f443a31065d09d80
abubakarm166 <143324758+abubakarm166@users.noreply.github.com>
2023-09-08 06:28:51 +0500

    Add files via upload

---
+++ car.js
@@ -0,0 +1,345 @@
+const menu= document.querySelector('.drop');
+const header=document.querySelector('.one');
+const close=document.querySelector('.fa-bars');
+const next=document.querySelector('.secc');
+const nav=document.querySelector('.no');
+const clo=document.querySelector('.fa-facebook');
+const nex=document.querySelector('.com2');
+const ppp=document.querySelector('.company-section');
+const car=document.querySelector('.oop');
+const footer=document.querySelector('.a');
+const to=document.querySelector('.to');
+const au=document.querySelector('.au');
+const ser=document.querySelector('.ser');
+const ose=document.querySelector('.fa-angle-down');
+const eee=document.querySelector('.com');
+const mob=document.querySelector('.mob');
+const mod=document.querySelector('.mod');
+const comse=document.querySelector('.company-list-mob');
+const hedding=document.querySelector('.perom');
+const dbl=document.querySelector('.pro-section');
+const investt=document.querySelector('.investt');
+const profile=document.querySelector('.pero');
+const in2o=document.querySelector('.in2o');
+const mainsus=document.querySelector('.mainsus');
+const sus=document.querySelector('.sus');
+const ress=document.querySelector('.ress');
+const rese=document.querySelector('.rese');
+const wark=document.querySelector('.wark');
+const works=document.querySelector('.works');
+const is=document.querySelector('.is');
+const wors=document.querySelector('.wors');
+const sut=document.querySelector('.sut');
+const student=document.querySelector('.student');
+const gra=document.querySelector('.gra');
+const graa=document.querySelector('.graa');
+const prre=document.querySelector('.prre');
+const perr=document.querySelector('.perr');
+const search=document.querySelector('.seer');
+const mana=document.querySelector('.mana');
+const manu=document.querySelector('.manu');
+
+
+
+function zoo() {
+	// body...
+	header.classList.toggle("inin");
+}
+
+
+
+search.addEventListener('click',zoo);
+
+
+
+
+
+
+
+
+
+
+
+
+function show() {
+	header.classList.toggle("active");
+  close.classList.toggle("fa-xmark")
+
+}
+
+
+menu.addEventListener('click',show);
+
+function clos() {
+	//header.classList.toggle("active");
+	nav.classList.toggle('ys');
+	clo.classList.toggle("fa-xmark");
+}
+next.addEventListener('click',clos);
+
+
+
+
+
+function closs() {
+	//header.classList.toggle("active");
+	nav.classList.toggle('po');
+    ppp.classList.toggle('noo');
+ //  ppp.classList.toggle('memo')
+	
+	
+	
+}
+nex.addEventListener('click',closs);
+
+
+function clss() {
+	//header.classList.toggle("active");
+	nav.classList.toggle('lo');
+   
+	
+}
+car.addEventListener('click',clss);
+
+
+
+function ee() {
+	//header.classList.toggle("active");
+	footer.classList.toggle('a3');
+   
+	
+}
+to.addEventListener('click',ee);
+
+
+
+function audi() {
+	//header.classList.toggle("active");
+	footer.classList.toggle('a4');
+   
+	
+}
+au.addEventListener('click',audi);
+
+
+
+function servi() {
+	//header.classList.toggle("active");
+	footer.classList.toggle('a5');
+   
+	
+}
+ser.addEventListener('click',servi);
+
+
+
+
+function x() {
+	// body...
+	nav.classList.toggle('ol');
+}
+eee.addEventListener('click',x);
+
+
+
+
+
+
+
+
+
+
+function y() {
+	// body...
+	nav.classList.toggle('opp');
+}
+
+
+
+
+
+nex.addEventListener('click',y);
+
+
+
+
+
+
+
+
+
+
+function m() {
+	// body...
+
+	nav.classList.toggle('bmw');
+}
+
+
+
+
+
+
+car.addEventListener('click',m);
+
+
+
+
+
+
+
+
+
+
+
+
+function host() {
+	// body...
+
+//dbl.style.display='block';
+	nav.classList.toggle('prooo')
+}
+
+
+
+
+
+
+
+profile.addEventListener('click',host);
+hedding.addEventListener('click',host);
+
+
+function mol() {
+	// body...
+	nav.classList.toggle('investr')
+}
+
+
+
+
+function wcc() {
+	// body...
+	nav.classList.toggle('invstr');
+}
+
+investt.addEventListener('click',mol);
+
+in2o.addEventListener('click',mol);
+
+function susa() {
+	// body...
+	nav.classList.toggle('susnav');
+}
+
+
+mainsus.addEventListener('click',susa);
+sus.addEventListener('click',susa);
+
+function resu() {
+	// body...
+	nav.classList.toggle('res')
+}
+
+
+
+ress.addEventListener('click',resu);
+rese.addEventListener('click',resu);
+
+
+
+
+
+
+
+
+
+
+
+function rso() {
+	// body...
+	nav.classList.toggle('wok');
+}
+
+
+
+works.addEventListener('click',rso);
+
+wark.addEventListener('click',rso);
+
+
+function so() {
+	// body...
+	nav.classList.toggle('pre')
+}
+
+
+
+wors.addEventListener('click',so);
+
+is.addEventListener('click',so);
+
+
+
+function ro() {
+	// body...
+	nav.classList.toggle('stu')
+}
+
+
+
+
+
+
+
+sut.addEventListener('click',ro);
+
+student.addEventListener('click',ro);
+
+
+
+
+function grov() {
+	// body...
+	nav.classList.toggle('grm');
+}
+
+
+
+graa.addEventListener('click',grov);
+gra.addEventListener('click',grov);
+
+
+
+
+
+
+function paso() {
+	// body...
+	nav.classList.toggle('pr');
+}
+
+
+
+prre.addEventListener('click',paso);
+perr.addEventListener('click',paso);
+
+
+
+function pasi() {
+	// body...
+	nav.classList.toggle('maan')
+}
+
+
+
+
+mana.addEventListener('click',pasi);
+manu.addEventListener('click',pasi);
+
+
+
+
+
+
+
